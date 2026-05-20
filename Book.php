36c2608d3e4d4eac54f6a178d2b8218d83c7aba0
Laurea Gregori <gregoridelrio@gmail.com>
2026-05-20 20:33:23 +0200

    added class Book constructor and getters

--- Book.php
+++ Book.php
@@ -9,4 +9,24 @@ class Book
   private Genre $genre;
   private int $pages;
   private int $loanCount;
+
+  public function __construct(string $title, string $author, int $year, Genre $genre, int $pages, int $loanCount)
+  {
+    $this->title = $title;
+    $this->author = $author;
+    $this->year = $year;
+    $this->genre = $genre;
+    $this->pages = $pages;
+    $this->loanCount = $loanCount;
+  }
+
+  public function getTitle(): string
+  {
+    return $this->title;
+  }
+
+  public function getLoanCount(): int
+  {
+    return $this->loanCount;
+  }
 }
