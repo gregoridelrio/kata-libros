23c9c4a91b496d34afc0892333126c1e373da275
Laurea Gregori <gregoridelrio@gmail.com>
2026-05-20 20:25:12 +0200

    added book class atributes

--- Book.php
+++ Book.php
@@ -1,1 +1,12 @@
 <?php
+require 'Genre.php';
+
+class Book
+{
+  private string $title;
+  private string $author;
+  private int $year;
+  private Genre $genre;
+  private int $pages;
+  private int $loanCount;
+}
