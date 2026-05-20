d6bf0dae893d4d27407ca61b11ebeff0a489f96c
Laurea Gregori <gregoridelrio@gmail.com>
2026-05-20 20:42:56 +0200

    we can see results with php index.php in terminal

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+require 'Book.php';
+require 'Library.php';
+
+$library = new Library();
+$book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, Genre::Novel, 218, 5);
+$book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960, Genre::Novel, 281, 8);
+$library->addBook($book1);
+$library->addBook($book2);
+
+$mostBorrowed = $library->mostBorrowedBook();
+if ($mostBorrowed) {
+  echo "The most borrowed book is: " . $mostBorrowed->getTitle() . " with " . $mostBorrowed->getLoanCount() . " loans.";
+} else {
+  echo "No books in the library.";
+}
+
+$booksBefore2000 = $library->getBookYearBefore2000();
+echo "\nBooks published before 2000:\n";
+
+foreach ($booksBefore2000 as $book) {
+  echo "- " . $book->getTitle() . " (" . $book->getAuthor() . ") published in " . $book->getYear() . "\n";
+}
